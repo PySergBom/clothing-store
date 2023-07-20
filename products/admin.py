3c8ca3e682ed0575f534aded201603dcb9fc4719
PySergBom <bomeevserg@gmail.com>
2023-07-20 23:21:17 +0300

    add requirements and products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import ProductCategory, Products
+
+
+@admin.register(ProductCategory)
+class AdminProdCategory(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Products)
+class AdminProdCategory(admin.ModelAdmin):
+    list_display = ('name', 'description','price', 'quantity', 'category')
