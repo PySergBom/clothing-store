67614dcbd9a650c03a80e12ddd036a7014530a7a
PySergBom <bomeevserg@gmail.com>
2023-07-20 13:39:42 +0300

    first migration

--- products/models.py
+++ products/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.db.models import CASCADE
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
+
 
 class Products(models.Model):
     name = models.CharField(max_length=256)
@@ -10,4 +13,4 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
-    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE())
+    category = models.ForeignKey(to=ProductCategory, on_delete=CASCADE)
