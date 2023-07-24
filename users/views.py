8046cdc71bfbd1ab32e81f7b4de98d9058c212fb
PySergBom <bomeevserg@gmail.com>
2023-07-24 22:57:43 +0300

    complete forms, views, templates

--- users/views.py
+++ users/views.py
@@ -1,15 +1,13 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
-    print(request.method)
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
-
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
@@ -25,5 +23,30 @@ def login(request):
 
 
 def registration(request):
-    # context = {}
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Поздравляем, вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store профиль',
+               'form': form}
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
